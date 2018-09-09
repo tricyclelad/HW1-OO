b41858267e3779687249d0e4098c3e76b6228f54
Camron Martinez <camron.martinez@aggiemail.usu.edu>
2018-09-08 18:11:16 -0600

    Initial Commit
    Added starter code to repo with no changes to given code.

---
+++ Minimal (c-sharp)/Shapes/Validator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class Validator
+    {
+        public static void ValidateDouble(double value, string errorMessage)
+        {
+            if (double.IsInfinity(value) || double.IsNaN(value))
+                throw new ShapeException(errorMessage);
+        }
+
+        public static void ValidatePositiveDouble(double value, String errorMessage) 
+        {
+                ValidateDouble(value, errorMessage);
+                if (value<0)
+                    throw new ShapeException(errorMessage);
+        }
+    }
+}
---
+++ Minimal (c-sharp)/ShapesTests/ValidatorTest.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shapes;
+
+namespace ShapesTests
+{
+    [TestClass]
+    public class ValidatorTest
+    {
+        [TestMethod]
+        public void TestValidateDouble()
+        {
+            Validator.ValidateDouble(123, "Test message");
+            Validator.ValidateDouble(0.00123, "Test message");
+            Validator.ValidateDouble(-123, "Test message");
+            Validator.ValidateDouble(-0.00123, "Test messagee");
+
+            try
+            {
+                Validator.ValidateDouble(double.PositiveInfinity, "Invalid");
+                Assert.Fail("Expected exception not thrown");
+            }
+            catch (ShapeException ex)
+            {
+                Assert.AreEqual("Invalid", ex.Message);
+            }
+
+            try
+            {
+                Validator.ValidateDouble(double.NegativeInfinity, "Invalid");
+                Assert.Fail("Expected exception not thrown");
+            }
+            catch (ShapeException ex)
+            {
+                Assert.AreEqual("Invalid", ex.Message);
+            }
+
+            try
+            {
+                Validator.ValidateDouble(double.NaN, "Invalid");
+                Assert.Fail("Expected exception not thrown");
+            }
+            catch (ShapeException ex)
+            {
+                Assert.AreEqual("Invalid", ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void TestValidatePositiveDouble()
+        {
+            Validator.ValidatePositiveDouble(456, "Test message");
+            Validator.ValidatePositiveDouble(0.34523, "Test message");
+
+            try
+            {
+                Validator.ValidatePositiveDouble(-123, "Negative not allowed");
+                Assert.Fail("Expected exception not thrown");
+            }
+            catch (ShapeException ex)
+            {
+                Assert.AreEqual("Negative not allowed", ex.Message);
+            }
+
+            try
+            {
+                Validator.ValidatePositiveDouble(-0.123, "Negative not allowed");
+                Assert.Fail("Expected exception not thrown");
+            }
+            catch (ShapeException ex)
+            {
+                Assert.AreEqual("Negative not allowed", ex.Message);
+            }
+
+            try
+            {
+                Validator.ValidatePositiveDouble(double.PositiveInfinity, "Invalid");
+                Assert.Fail("Expected exception not thrown");
+            }
+            catch (ShapeException ex)
+            {
+                Assert.AreEqual("Invalid", ex.Message);
+            }
+
+            try
+            {
+                Validator.ValidatePositiveDouble(double.NegativeInfinity, "Invalid");
+                Assert.Fail("Expected exception not thrown");
+            }
+            catch (ShapeException ex)
+            {
+                Assert.AreEqual("Invalid", ex.Message);
+            }
+
+            try
+            {
+                Validator.ValidatePositiveDouble(double.NaN, "Invalid");
+                Assert.Fail("Expected exception not thrown");
+            }
+            catch (ShapeException ex)
+            {
+                Assert.AreEqual("Invalid", ex.Message);
+            }
+        }
+    }
+}
