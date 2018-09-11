81ad41065345470e0300dbcb7dd923f74e21d1af
Camron Martinez <camron.martinez@aggiemail.usu.edu>
2018-09-11 17:05:03 -0600

    Rectangle class finished
    Added all requirements to Rectangle class

--- Minimal (c-sharp)/ShapesTests/RectangleTest.cs
+++ Minimal (c-sharp)/ShapesTests/RectangleTest.cs
@@ -14,6 +14,8 @@ namespace ShapesTests
             var p2 = new Point(0, 1);
             var p3 = new Point(1, 1);
             var p4 = new Point(1, 0);
+            var p5 = new Point(2, 0);
+            var p6 = new Point(2, 1);
             //var myRectangle = new Rectangle(p1,p2,p3,p4);
             var myRectangle = new Rectangle(0, 0, 0, 1, 1, 1, 1, 0);
 
@@ -28,86 +30,168 @@ namespace ShapesTests
             try
             {
                 myRectangle = new Rectangle(0, 0, 1, 1, 0, 0, 1, 1);
-        Assert.Fail("Expected exception not thrown");
+                Assert.Fail("Expected exception not thrown");
             }
             catch (ShapeException e)
             {
                 Assert.AreEqual("Two or more Rectangle Vertices are equal", e.Message);
             }
+            try
+            {
+                myRectangle = new Rectangle(0, 0, 0, 1, 1, 1, 2, 0);
+                Assert.Fail("Expected exception not thrown");
+            }
+            catch (ShapeException e)
+            {
+                Assert.AreEqual("Two or more corresponding sides are not equal lengths", e.Message);
+            }
+
+            try
+            {
+                myRectangle = new Rectangle(0, 0, 1, 1, 2, 1, 1, 0);
+                Assert.Fail("Expected exception not thrown");
+            }
+            catch (ShapeException e)
+            {
+                Assert.AreEqual("Atleast one vertex is not squared with the other vertices", e.Message);
+            }
+            try
+            {
+                myRectangle = new Rectangle(p1, p2, p1, p3);
+                Assert.Fail("Expected exception not thrown");
+            }
+            catch (ShapeException e)
+            {
+                Assert.AreEqual("Two or more Rectangle Vertices are equal", e.Message);
+            }
+            try
+            {
+                myRectangle = new Rectangle(p1, p1, p1, p1);
+                Assert.Fail("Expected exception not thrown");
+            }
+            catch (ShapeException e)
+            {
+                Assert.AreEqual("Two or more Rectangle Vertices are equal", e.Message);
+            }
+
+            try
+            {
+                myRectangle = new Rectangle(p1, p2, p3, p5);
+                Assert.Fail("Expected exception not thrown");
+            }
+            catch (ShapeException e)
+            {
+                Assert.AreEqual("Two or more corresponding sides are not equal lengths", e.Message);
+            }
+            try
+            {
+                myRectangle = new Rectangle(p1, p3, p6, p4);
+                Assert.Fail("Expected exception not thrown");
+            }
+            catch (ShapeException e)
+            {
+                Assert.AreEqual("Atleast one vertex is not squared with the other vertices", e.Message);
+            }
         }
-        //    try
-        //    {
-        //        myRectangle = new Rectangle(0, 0, 0, 1, 1, 1, 2, 0);
-        //        Assert.Fail("Expected exception not thrown");
-        //    }
-        //    catch (ShapeException e)
-        //    {
-        //        Assert.AreEqual("Two or more corresponding sides are not equal lengths", e.Message);
-        //    }
+        [TestMethod]
+        public void TestComputeArea()
+        {
+            Rectangle myRectangle = new Rectangle(0, 0, 0, 1, 1, 1, 1, 0);
+            Assert.AreEqual(1, myRectangle.ComputeArea(), 0.0001);
 
-        //    try
-        //    {
-        //        myRectangle = new Rectangle(0, 0, 1, 1, 2, 1, 1, 0);
-        //        Assert.Fail("Expected exception not thrown");
-        //    }
-        //    catch (ShapeException e)
-        //    {
-        //        Assert.AreEqual("Atleast one vertex is not squared with the other vertices", e.Message);
-        //    }
-        //}
-        //    try
-        //    {
-        //        myTriangle = new Triangle(p1, p2, p1);
-        //    }
-        //    catch (ShapeException e)
-        //    {
-        //        Assert.AreEqual("Two or more Triangle Vertices are equal", e.Message);
-        //    }
+            myRectangle = new Rectangle(0, 0, 0, 5.5, 10, 5.5, 10, 0);
+            Assert.AreEqual(55, myRectangle.ComputeArea(), 0.0001);
+        }
+        [TestMethod]
+        public void TestMove()
+        {
+            Rectangle myRectangle = new Rectangle(0, 0, 0, 1, 1, 1, 1, 0);
+            Assert.AreEqual(0, myRectangle.Vertex1.X, 0);
+            Assert.AreEqual(0, myRectangle.Vertex1.Y, 0);
+            Assert.AreEqual(0, myRectangle.Vertex2.X, 0);
+            Assert.AreEqual(1, myRectangle.Vertex2.Y, 0);
+            Assert.AreEqual(1, myRectangle.Vertex3.X, 0);
+            Assert.AreEqual(1, myRectangle.Vertex3.Y, 0);
+            Assert.AreEqual(1, myRectangle.Vertex4.X, 0);
+            Assert.AreEqual(0, myRectangle.Vertex4.Y, 0);
 
-        //    try
-        //    {
-        //        myTriangle = new Triangle(p1, p1, p1);
-        //    }
-        //    catch (ShapeException e)
-        //    {
-        //        Assert.AreEqual("Two or more Triangle Vertices are equal", e.Message);
-        //    }
+            myRectangle.Move(10, 10);
+            Assert.AreEqual(10, myRectangle.Vertex1.X, 0);
+            Assert.AreEqual(10, myRectangle.Vertex1.Y, 0);
+            Assert.AreEqual(10, myRectangle.Vertex2.X, 0);
+            Assert.AreEqual(11, myRectangle.Vertex2.Y, 0);
+            Assert.AreEqual(11, myRectangle.Vertex3.X, 0);
+            Assert.AreEqual(11, myRectangle.Vertex3.Y, 0);
+            Assert.AreEqual(11, myRectangle.Vertex4.X, 0);
+            Assert.AreEqual(10, myRectangle.Vertex4.Y, 0);
 
-        //    try
-        //    {
-        //        myTriangle = new Triangle(p2, p1, p1);
-        //    }
-        //    catch (ShapeException e)
-        //    {
-        //        Assert.AreEqual("Two or more Triangle Vertices are equal", e.Message);
-        //    }
+            myRectangle.Move(.123, .123);
+            Assert.AreEqual(10.123, myRectangle.Vertex1.X, 0);
+            Assert.AreEqual(10.123, myRectangle.Vertex1.Y, 0);
+            Assert.AreEqual(10.123, myRectangle.Vertex2.X, 0);
+            Assert.AreEqual(11.123, myRectangle.Vertex2.Y, 0);
+            Assert.AreEqual(11.123, myRectangle.Vertex3.X, 0);
+            Assert.AreEqual(11.123, myRectangle.Vertex3.Y, 0);
+            Assert.AreEqual(11.123, myRectangle.Vertex4.X, 0);
+            Assert.AreEqual(10.123, myRectangle.Vertex4.Y, 0);
+            try
+            {
+                myRectangle.Move(double.PositiveInfinity, 1);
+                Assert.Fail("Expected exception not thrown");
+            }
+            catch (ShapeException e)
+            {
+                Assert.AreEqual("Invalid delta-x value", e.Message);
+            }
+            try
+            {
+                myRectangle.Move(double.NegativeInfinity, 1);
+                Assert.Fail("Expected exception not thrown");
+            }
+            catch (ShapeException e)
+            {
+                Assert.AreEqual("Invalid delta-x value", e.Message);
+            }
 
+            try
+            {
+                myRectangle.Move(double.NaN, 1);
+                Assert.Fail("Expected exception not thrown");
+            }
+            catch (ShapeException e)
+            {
+                Assert.AreEqual("Invalid delta-x value", e.Message);
+            }
 
-        //    try
-        //    {
-        //        myTriangle = new Triangle(p1, p2, p4);
-        //        Assert.Fail("Expected exception not thrown");
-        //    }
-        //    catch (ShapeException e)
-        //    {
-        //        Assert.AreEqual("Invalid triangle edges: 1 or more lines are in same location", e.Message);
-        //    }
-        //    p1 = new Point(1.4, 2.5);
-        //    p2 = new Point(4.6, 10.7);
-        //    p3 = new Point(5.6, 13.7);
-        //    myTriangle = new Triangle(p1, p2, p3);
-        //    Assert.AreSame(p1, myTriangle.Vertex1);
-        //    Assert.AreSame(p2, myTriangle.Vertex2);
-        //    Assert.AreSame(p3, myTriangle.Vertex3);
+            try
+            {
+                myRectangle.Move(1, double.PositiveInfinity);
+                Assert.Fail("Expected exception not thrown");
+            }
+            catch (ShapeException e)
+            {
+                Assert.AreEqual("Invalid delta-y value", e.Message);
+            }
 
-        //    myTriangle = new Triangle(0.1, 0.1, 1.1, 1.1, 2.1, 0.1);
+            try
+            {
+                myRectangle.Move(1, double.PositiveInfinity);
+                Assert.Fail("Expected exception not thrown");
+            }
+            catch (ShapeException e)
+            {
+                Assert.AreEqual("Invalid delta-y value", e.Message);
+            }
 
-        //    Assert.AreEqual(0.1, myTriangle.Vertex1.X, 0);
-        //    Assert.AreEqual(0.1, myTriangle.Vertex1.Y, 0);
-        //    Assert.AreEqual(1.1, myTriangle.Vertex2.X, 0);
-        //    Assert.AreEqual(1.1, myTriangle.Vertex2.Y, 0);
-        //    Assert.AreEqual(2.1, myTriangle.Vertex3.X, 0);
-        //    Assert.AreEqual(0.1, myTriangle.Vertex3.Y, 0);
-        //}
+            try
+            {
+                myRectangle.Move(1, double.NaN);
+                Assert.Fail("Expected exception not thrown");
+            }
+            catch (ShapeException e)
+            {
+                Assert.AreEqual("Invalid delta-y value", e.Message);
+            }
+        }
     }
 }
