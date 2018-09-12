b954dbc290a335a3c76172956372046406ac5700
Camron Martinez <camron.martinez@aggiemail.usu.edu>
2018-09-11 20:52:36 -0600

    Ellipse: Unit tests for first constructor
    Created unit tests for first constructor

--- Minimal (c-sharp)/Shapes/Ellipse.cs
+++ Minimal (c-sharp)/Shapes/Ellipse.cs
@@ -92,92 +92,167 @@ namespace Shapes
 
 
         }
-        //Create an Ellipse with vertices of a binding rectangle
-        public Ellipse(Point _Vertex1, Point _Vertex2, Point _Vertex3, Point _Vertex4)
-        {
-            Rectangle _BindingRectangle = new Rectangle(_Vertex1, _Vertex2, _Vertex3, _Vertex4);
-            BindingRectangle = _BindingRectangle;
-            Line Diagonal = new Line(BindingRectangle.Vertex1, BindingRectangle.Vertex3);
-            Point CenterOfDiagonal = new Point(((Diagonal.Point1.X + Diagonal.Point2.X)/2), ((Diagonal.Point1.Y + Diagonal.Point2.Y)/2));
-            Point CenterOfRectangleEdge1 = new Point(((BindingRectangle.Vertex1.X + BindingRectangle.Vertex2.X)/2), ((BindingRectangle.Vertex1.Y + BindingRectangle.Vertex2.Y)/2));
-            Point CenterOfRectangleEdge2 = new Point(((BindingRectangle.Vertex2.X + BindingRectangle.Vertex3.X)/2), ((BindingRectangle.Vertex2.Y + BindingRectangle.Vertex3.Y)/2));
-            Point CenterOfRectangleEdge3 = new Point(((BindingRectangle.Vertex3.X + BindingRectangle.Vertex4.X)/2), ((BindingRectangle.Vertex3.Y + BindingRectangle.Vertex4.Y)/2));
-            Point CenterOfRectangleEdge4 = new Point(((BindingRectangle.Vertex4.X + BindingRectangle.Vertex1.X)/2), ((BindingRectangle.Vertex4.Y + BindingRectangle.Vertex1.Y)/2));
+        ////Create an Ellipse with vertices of a binding rectangle
+        //public Ellipse(Point _Vertex1, Point _Vertex2, Point _Vertex3, Point _Vertex4)
+        //{
+        //    Rectangle _BindingRectangle = new Rectangle(_Vertex1, _Vertex2, _Vertex3, _Vertex4);
+        //    BindingRectangle = _BindingRectangle;
+        //    Line Diagonal = new Line(BindingRectangle.Vertex1, BindingRectangle.Vertex3);
+        //    Point CenterOfDiagonal = new Point(((Diagonal.Point1.X + Diagonal.Point2.X)/2), ((Diagonal.Point1.Y + Diagonal.Point2.Y)/2));
+        //    Point CenterOfRectangleEdge1 = new Point(((BindingRectangle.Vertex1.X + BindingRectangle.Vertex2.X)/2), ((BindingRectangle.Vertex1.Y + BindingRectangle.Vertex2.Y)/2));
+        //    Point CenterOfRectangleEdge2 = new Point(((BindingRectangle.Vertex2.X + BindingRectangle.Vertex3.X)/2), ((BindingRectangle.Vertex2.Y + BindingRectangle.Vertex3.Y)/2));
+        //    Point CenterOfRectangleEdge3 = new Point(((BindingRectangle.Vertex3.X + BindingRectangle.Vertex4.X)/2), ((BindingRectangle.Vertex3.Y + BindingRectangle.Vertex4.Y)/2));
+        //    Point CenterOfRectangleEdge4 = new Point(((BindingRectangle.Vertex4.X + BindingRectangle.Vertex1.X)/2), ((BindingRectangle.Vertex4.Y + BindingRectangle.Vertex1.Y)/2));
 
-            Line _Axis1 = new Line(CenterOfRectangleEdge1, CenterOfRectangleEdge3);
-            Line _Axis2 = new Line(CenterOfRectangleEdge2, CenterOfRectangleEdge4);
+        //    Line _Axis1 = new Line(CenterOfRectangleEdge1, CenterOfRectangleEdge3);
+        //    Line _Axis2 = new Line(CenterOfRectangleEdge2, CenterOfRectangleEdge4);
 
-            Center =  CenterOfDiagonal;
-            Vertex1 = CenterOfRectangleEdge1;
-            Vertex2 = CenterOfRectangleEdge2;
-            Vertex3 = CenterOfRectangleEdge3;
-            Vertex4 = CenterOfRectangleEdge4;
-            Axis1 = _Axis1;
-            Axis2 = _Axis2;
+        //    Center =  CenterOfDiagonal;
+        //    Vertex1 = CenterOfRectangleEdge1;
+        //    Vertex2 = CenterOfRectangleEdge2;
+        //    Vertex3 = CenterOfRectangleEdge3;
+        //    Vertex4 = CenterOfRectangleEdge4;
+        //    Axis1 = _Axis1;
+        //    Axis2 = _Axis2;
 
-            if (Axis1.ComputeLength() > Axis2.ComputeLength())
-            {
-                //Axis1 is major axis
-                if (CenterOfRectangleEdge1.X == CenterOfRectangleEdge3.X)
-                {
-                    //foci are on the y axis
-                    double FociDistanceFromCenter = Math.Sqrt(((Axis1.ComputeLength() / 2) * (Axis1.ComputeLength() / 2)) - ((Axis2.ComputeLength() / 2) * (Axis2.ComputeLength() / 2)));
-                    Point _foci1 = new Point(CenterOfRectangleEdge1.X, Center.Y - FociDistanceFromCenter);
-                    Point _foci2 = new Point(CenterOfRectangleEdge1.X, Center.Y + FociDistanceFromCenter);
-                    Foci1 = _foci1;
-                    Foci2 = _foci2;
-                }
-                else
-                {
-                    //Foci are on the x axis 
-                    double FociDistanceFromCenter = Math.Sqrt(((Axis1.ComputeLength() / 2) * (Axis1.ComputeLength() / 2)) - ((Axis2.ComputeLength() / 2) * (Axis2.ComputeLength() / 2)));
-                    Point _foci1 = new Point(Center.X - FociDistanceFromCenter, CenterOfRectangleEdge1.Y);
-                    Point _foci2 = new Point(Center.X + FociDistanceFromCenter, CenterOfRectangleEdge1.Y);
-                    Foci1 = _foci1;
-                    Foci2 = _foci2;
-                }
-            }
-            else
-            {
-                //Axis2 is major axis
-                 if (CenterOfRectangleEdge1.X == CenterOfRectangleEdge3.X)
-                {
-                    //foci are on the y axis
-                    double FociDistanceFromCenter = Math.Sqrt(((Axis2.ComputeLength() / 2) * (Axis2.ComputeLength() / 2)) - ((Axis1.ComputeLength() / 2) * (Axis1.ComputeLength() / 2)));
-                    Point _foci1 = new Point(CenterOfRectangleEdge1.X, Center.Y - FociDistanceFromCenter);
-                    Point _foci2 = new Point(CenterOfRectangleEdge1.X, Center.Y + FociDistanceFromCenter);
-                    Foci1 = _foci1;
-                    Foci2 = _foci2;
+        //    if (Axis1.ComputeLength() > Axis2.ComputeLength())
+        //    {
+        //        //Axis1 is major axis
+        //        if (CenterOfRectangleEdge1.X == CenterOfRectangleEdge3.X)
+        //        {
+        //            //foci are on the y axis
+        //            double FociDistanceFromCenter = Math.Sqrt(((Axis1.ComputeLength() / 2) * (Axis1.ComputeLength() / 2)) - ((Axis2.ComputeLength() / 2) * (Axis2.ComputeLength() / 2)));
+        //            Point _foci1 = new Point(CenterOfRectangleEdge1.X, Center.Y - FociDistanceFromCenter);
+        //            Point _foci2 = new Point(CenterOfRectangleEdge1.X, Center.Y + FociDistanceFromCenter);
+        //            Foci1 = _foci1;
+        //            Foci2 = _foci2;
+        //        }
+        //        else
+        //        {
+        //            //Foci are on the x axis 
+        //            double FociDistanceFromCenter = Math.Sqrt(((Axis1.ComputeLength() / 2) * (Axis1.ComputeLength() / 2)) - ((Axis2.ComputeLength() / 2) * (Axis2.ComputeLength() / 2)));
+        //            Point _foci1 = new Point(Center.X - FociDistanceFromCenter, CenterOfRectangleEdge1.Y);
+        //            Point _foci2 = new Point(Center.X + FociDistanceFromCenter, CenterOfRectangleEdge1.Y);
+        //            Foci1 = _foci1;
+        //            Foci2 = _foci2;
+        //        }
+        //    }
+        //    else
+        //    {
+        //        //Axis2 is major axis
+        //         if (CenterOfRectangleEdge1.X == CenterOfRectangleEdge3.X)
+        //        {
+        //            //foci are on the y axis
+        //            double FociDistanceFromCenter = Math.Sqrt(((Axis2.ComputeLength() / 2) * (Axis2.ComputeLength() / 2)) - ((Axis1.ComputeLength() / 2) * (Axis1.ComputeLength() / 2)));
+        //            Point _foci1 = new Point(CenterOfRectangleEdge1.X, Center.Y - FociDistanceFromCenter);
+        //            Point _foci2 = new Point(CenterOfRectangleEdge1.X, Center.Y + FociDistanceFromCenter);
+        //            Foci1 = _foci1;
+        //            Foci2 = _foci2;
 
-                }
-                else
-                {
-                    //Focis are on the x axis 
-                    double FociDistanceFromCenter = Math.Sqrt(((Axis2.ComputeLength() / 2) * (Axis2.ComputeLength() / 2)) - ((Axis1.ComputeLength() / 2) * (Axis1.ComputeLength() / 2)));
-                    Point _foci1 = new Point(Center.X - FociDistanceFromCenter, CenterOfRectangleEdge1.Y);
-                    Point _foci2 = new Point(Center.X + FociDistanceFromCenter, CenterOfRectangleEdge1.Y);
-                    Foci1 = _foci1;
-                    Foci2 = _foci2;
-                }   
+        //        }
+        //        else
+        //        {
+        //            //Focis are on the x axis 
+        //            double FociDistanceFromCenter = Math.Sqrt(((Axis2.ComputeLength() / 2) * (Axis2.ComputeLength() / 2)) - ((Axis1.ComputeLength() / 2) * (Axis1.ComputeLength() / 2)));
+        //            Point _foci1 = new Point(Center.X - FociDistanceFromCenter, CenterOfRectangleEdge1.Y);
+        //            Point _foci2 = new Point(Center.X + FociDistanceFromCenter, CenterOfRectangleEdge1.Y);
+        //            Foci1 = _foci1;
+        //            Foci2 = _foci2;
+        //        }   
 
-            }
-        }
-        public void MoveEllipse(double deltaX, double deltaY)
-        {
-            Center.Move(deltaX, deltaY); 
-            Vertex1.Move(deltaX, deltaY); 
-            Vertex2.Move(deltaX, deltaY); 
-            Vertex3.Move(deltaX, deltaY); 
-            Vertex4.Move(deltaX, deltaY);
-            Foci1.Move(deltaX, deltaY);
-            Foci2.Move(deltaX, deltaY);
-            Axis1.Move(deltaX, deltaY);
-            Axis2.Move(deltaX, deltaY);
-            BindingRectangle.Move(deltaX, deltaY);
-        }
-         public double ComputeArea()
-        {
-            return Math.PI * Axis1.ComputeLength()/2 * Axis2.ComputeLength()/2;
-        }
+        //    }
+        //}
+
+        //public Ellipse(double _x1, double _y1,double _x2,double _y2,double _x3,double _y3,double _x4, double _y4)
+        //{
+        //    Point _Vertex1 = new Point(_x1, _y1);
+        //    Point _Vertex2 = new Point(_x1, _y1);
+        //    Point _Vertex3 = new Point(_x1, _y1);
+        //    Point _Vertex4 = new Point(_x1, _y1);
+
+        //    Rectangle _BindingRectangle = new Rectangle(_Vertex1, _Vertex2, _Vertex3, _Vertex4);
+        //    BindingRectangle = _BindingRectangle;
+        //    Line Diagonal = new Line(BindingRectangle.Vertex1, BindingRectangle.Vertex3);
+        //    Point CenterOfDiagonal = new Point(((Diagonal.Point1.X + Diagonal.Point2.X)/2), ((Diagonal.Point1.Y + Diagonal.Point2.Y)/2));
+        //    Point CenterOfRectangleEdge1 = new Point(((BindingRectangle.Vertex1.X + BindingRectangle.Vertex2.X)/2), ((BindingRectangle.Vertex1.Y + BindingRectangle.Vertex2.Y)/2));
+        //    Point CenterOfRectangleEdge2 = new Point(((BindingRectangle.Vertex2.X + BindingRectangle.Vertex3.X)/2), ((BindingRectangle.Vertex2.Y + BindingRectangle.Vertex3.Y)/2));
+        //    Point CenterOfRectangleEdge3 = new Point(((BindingRectangle.Vertex3.X + BindingRectangle.Vertex4.X)/2), ((BindingRectangle.Vertex3.Y + BindingRectangle.Vertex4.Y)/2));
+        //    Point CenterOfRectangleEdge4 = new Point(((BindingRectangle.Vertex4.X + BindingRectangle.Vertex1.X)/2), ((BindingRectangle.Vertex4.Y + BindingRectangle.Vertex1.Y)/2));
+
+        //    Line _Axis1 = new Line(CenterOfRectangleEdge1, CenterOfRectangleEdge3);
+        //    Line _Axis2 = new Line(CenterOfRectangleEdge2, CenterOfRectangleEdge4);
+
+        //    Center =  CenterOfDiagonal;
+        //    Vertex1 = CenterOfRectangleEdge1;
+        //    Vertex2 = CenterOfRectangleEdge2;
+        //    Vertex3 = CenterOfRectangleEdge3;
+        //    Vertex4 = CenterOfRectangleEdge4;
+        //    Axis1 = _Axis1;
+        //    Axis2 = _Axis2;
+
+        //    if (Axis1.ComputeLength() > Axis2.ComputeLength())
+        //    {
+        //        //Axis1 is major axis
+        //        if (CenterOfRectangleEdge1.X == CenterOfRectangleEdge3.X)
+        //        {
+        //            //foci are on the y axis
+        //            double FociDistanceFromCenter = Math.Sqrt(((Axis1.ComputeLength() / 2) * (Axis1.ComputeLength() / 2)) - ((Axis2.ComputeLength() / 2) * (Axis2.ComputeLength() / 2)));
+        //            Point _foci1 = new Point(CenterOfRectangleEdge1.X, Center.Y - FociDistanceFromCenter);
+        //            Point _foci2 = new Point(CenterOfRectangleEdge1.X, Center.Y + FociDistanceFromCenter);
+        //            Foci1 = _foci1;
+        //            Foci2 = _foci2;
+        //        }
+        //        else
+        //        {
+        //            //Foci are on the x axis 
+        //            double FociDistanceFromCenter = Math.Sqrt(((Axis1.ComputeLength() / 2) * (Axis1.ComputeLength() / 2)) - ((Axis2.ComputeLength() / 2) * (Axis2.ComputeLength() / 2)));
+        //            Point _foci1 = new Point(Center.X - FociDistanceFromCenter, CenterOfRectangleEdge1.Y);
+        //            Point _foci2 = new Point(Center.X + FociDistanceFromCenter, CenterOfRectangleEdge1.Y);
+        //            Foci1 = _foci1;
+        //            Foci2 = _foci2;
+        //        }
+        //    }
+        //    else
+        //    {
+        //        //Axis2 is major axis
+        //         if (CenterOfRectangleEdge1.X == CenterOfRectangleEdge3.X)
+        //        {
+        //            //foci are on the y axis
+        //            double FociDistanceFromCenter = Math.Sqrt(((Axis2.ComputeLength() / 2) * (Axis2.ComputeLength() / 2)) - ((Axis1.ComputeLength() / 2) * (Axis1.ComputeLength() / 2)));
+        //            Point _foci1 = new Point(CenterOfRectangleEdge1.X, Center.Y - FociDistanceFromCenter);
+        //            Point _foci2 = new Point(CenterOfRectangleEdge1.X, Center.Y + FociDistanceFromCenter);
+        //            Foci1 = _foci1;
+        //            Foci2 = _foci2;
+
+        //        }
+        //        else
+        //        {
+        //            //Focis are on the x axis 
+        //            double FociDistanceFromCenter = Math.Sqrt(((Axis2.ComputeLength() / 2) * (Axis2.ComputeLength() / 2)) - ((Axis1.ComputeLength() / 2) * (Axis1.ComputeLength() / 2)));
+        //            Point _foci1 = new Point(Center.X - FociDistanceFromCenter, CenterOfRectangleEdge1.Y);
+        //            Point _foci2 = new Point(Center.X + FociDistanceFromCenter, CenterOfRectangleEdge1.Y);
+        //            Foci1 = _foci1;
+        //            Foci2 = _foci2;
+        //        }   
+
+        //    }
+        //}
+        //public void MoveEllipse(double deltaX, double deltaY)
+        //{
+        //    Center.Move(deltaX, deltaY); 
+        //    Vertex1.Move(deltaX, deltaY); 
+        //    Vertex2.Move(deltaX, deltaY); 
+        //    Vertex3.Move(deltaX, deltaY); 
+        //    Vertex4.Move(deltaX, deltaY);
+        //    Foci1.Move(deltaX, deltaY);
+        //    Foci2.Move(deltaX, deltaY);
+        //    Axis1.Move(deltaX, deltaY);
+        //    Axis2.Move(deltaX, deltaY);
+        //    BindingRectangle.Move(deltaX, deltaY);
+        //}
+        // public double ComputeArea()
+        //{
+        //    return Math.PI * Axis1.ComputeLength()/2 * Axis2.ComputeLength()/2;
+        //}
     }
 }
--- Minimal (c-sharp)/ShapesTests/EllipseTest.cs
+++ Minimal (c-sharp)/ShapesTests/EllipseTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Shapes;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ShapesTests
@@ -7,8 +8,50 @@ namespace ShapesTests
     public class EllipseTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestValidConstruction()
         {
+            Rectangle myRectangle = new Rectangle(-2, -1, -2, 1, 2, 1, 2, -1);
+            var Ellipse = new Ellipse(myRectangle);
+            //var Ellipse = new Ellipse(-2, -1, -2, 1, 2, 1, 2, -1);
+
+            Assert.AreEqual(0, Ellipse.Center.X);
+            Assert.AreEqual(0, Ellipse.Center.Y);
+            Assert.AreEqual(-1.73205080757, Ellipse.Foci1.X, 0.0001);
+            Assert.AreEqual(0, Ellipse.Foci1.Y);
+            Assert.AreEqual(1.73205080757, Ellipse.Foci2.X, 0.0001);
+            Assert.AreEqual(0, Ellipse.Foci2.Y);
+
+            Assert.AreEqual(-2, Ellipse.Vertex1.X);
+            Assert.AreEqual(0, Ellipse.Vertex1.Y);
+            Assert.AreEqual(0, Ellipse.Vertex2.X);
+            Assert.AreEqual(1, Ellipse.Vertex2.Y);
+            Assert.AreEqual(2, Ellipse.Vertex3.X);
+            Assert.AreEqual(0, Ellipse.Vertex3.Y);
+            Assert.AreEqual(0, Ellipse.Vertex4.X);
+            Assert.AreEqual(-1, Ellipse.Vertex4.Y);
+
+            Line _Axis1 = new Line(-2, 0, 2, 0);
+            Line _Axis2 = new Line(0, 1, 0, -1);
+
+            Assert.AreEqual(_Axis1.Point1.X, Ellipse.Axis1.Point1.X);
+            Assert.AreEqual(_Axis1.Point1.Y, Ellipse.Axis1.Point1.Y);
+            Assert.AreEqual(_Axis1.Point2.X, Ellipse.Axis1.Point2.X);
+            Assert.AreEqual(_Axis1.Point2.Y, Ellipse.Axis1.Point2.Y);
+
+            Assert.AreEqual(_Axis2.Point1.X, Ellipse.Axis2.Point1.X);
+            Assert.AreEqual(_Axis2.Point1.Y, Ellipse.Axis2.Point1.Y);
+            Assert.AreEqual(_Axis2.Point2.X, Ellipse.Axis2.Point2.X);
+            Assert.AreEqual(_Axis2.Point2.Y, Ellipse.Axis2.Point2.Y);
+
+            Rectangle _BindingRectangle = new Rectangle(-2, -1, -2, 1, 2, 1, 2, -1);
+            Assert.AreEqual(_BindingRectangle.Vertex1.X, Ellipse.BindingRectangle.Vertex1.X);
+            Assert.AreEqual(_BindingRectangle.Vertex1.Y, Ellipse.BindingRectangle.Vertex1.Y);
+            Assert.AreEqual(_BindingRectangle.Vertex2.X, Ellipse.BindingRectangle.Vertex2.X);
+            Assert.AreEqual(_BindingRectangle.Vertex2.Y, Ellipse.BindingRectangle.Vertex2.Y);
+            Assert.AreEqual(_BindingRectangle.Vertex3.X, Ellipse.BindingRectangle.Vertex3.X);
+            Assert.AreEqual(_BindingRectangle.Vertex3.Y, Ellipse.BindingRectangle.Vertex3.Y);
+            Assert.AreEqual(_BindingRectangle.Vertex4.X, Ellipse.BindingRectangle.Vertex4.X);
+            Assert.AreEqual(_BindingRectangle.Vertex4.Y, Ellipse.BindingRectangle.Vertex4.Y);
         }
     }
 }
